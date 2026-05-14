d389675f91d6ebd6df82e69ba5a62f0ecd5cee85
Florian Thiel <flth@flth.de>
2026-05-14 19:10:02 +0200

    initial

---
+++ strings.lua
@@ -0,0 +1,74 @@
+function data()
+return {
+	en = {
+		["mod_name"] = "DB ET 91 / Class 491",
+		["mod_desc"] = "The vehicles of the series ET 91 of the Deutsche Reichsbahn (DRG) were electric multiple units for special traffic. The vehicles, also known as the Glass Train, were glazed all around and on the roof sections. This gave the passengers a special all-round view. They were only used for excursions and special trips, especially in southern Germany and Austria. After the war one railcar could be taken over by the Deutsche Bundesbahn. From 1968 it was listed as BR 491 001, after an accident in 1995 it was taken out of service and is today in the museum.",
+
+		["ET91_menu"] = "ET 91 / Class 491",
+        ["towed"] = " [towed]",
+		["elT1998_name"] = "elT 1998 (1935)",
+		["elT1998_desc"] = "The railcars were built in 1935 in two copies as elT in 1998 and 1999 by Waggonfabrik Fuchs and AEG. From 1940 the series was run as the ET 91. ET 91 01 was walled up in a small locomotive shed during the war and thus remained operational.",
+        ["elT1999_name"] = "elT 1999 (1935)",
+		["elT1999_desc"] = "The railcars were built in 1935 in two copies as elT in 1998 and 1999 by Waggonfabrik Fuchs and AEG. From 1940 the series was run as the ET 91. ET 91 02 burned out completely during a bombing raid on the Munich railway depot on March 9, 1943 and was then retired on July 9, 1943.",
+
+		["ET91_49_name"] = "ET 91 01 (1949)",
+		["ET91_49_desc"] = "The vehicle was almost undamaged and came to the Deutsche Bundesbahn after the Second World War. It was refurbished and repainted and used again from Munich Hbf.",
+
+		["ET91_53_name"] = "ET 91 01 (1953)",
+		["ET91_53_desc"] = "In 1953 the ET 91 was rebuilt, among other things the pantograph and the electrical part were renewed. A third headlights were added and the paintwork was renewed.",
+
+		["ET91_68_name"] = "491 001-4 (1968)",
+		["ET91_68_desc"] = "With the introduction of the EDP numbers, the ET 91 was given the series designation 491. Since there was only one multiple unit, there was only the number 491 001-4.",
+
+        ["ET91_72_towed_name"] = "491 001-4 (1972)",
+		["ET91_72_name"] = "491 001-4 [DE] (1972)",
+		["ET91_72_desc"] = "For the Olympics in Germany, the ET 91 now listed under EDV 491 was given a new color scheme in Olympic blue. This should underline the special status of the train. In addition, a pantograph was added for operation in Switzerland, as well as new lamps.\nLifted double-arm pantograph for operation in DE and AT.",
+        ["ET91_72_CH_name"] = "491 001-4 [CH] (1972)",
+		["ET91_72_CH_desc"] = "For the Olympics in Germany, the ET 91 now listed under EDV 491 was given a new color scheme in Olympic blue. This should underline the special status of the train. In addition, a pantograph was added for operation in Switzerland, as well as new lamps.\nLifted single-arm pantograph for operation in Switzerland.",
+
+        ["ET91_86_towed_name"] = "491 001-4 (1986)",
+		["ET91_86_name"] = "491 001-4 [DE] (1986)",
+		["ET91_86_desc"] = "From 1977, consideration was given to a new paintwork, but it took a long time until the revision in 1986: The Olympic blue color applied in 1972 was no longer up-to-date. In the end, the decision was made in favor of the color gentian blue, which emphasizes the Bavarian home of the Glass Train. In 1995 the train was involved in an accident at Garmisch-Partenkirchen station and was then taken out of service and has since been parked in the Augsburg railway park in a non-operational state.\nLifted double-arm pantograph for operation in DE and AT.",
+        ["ET91_86_CH_name"] = "491 001-4 [CH] (1986)",
+		["ET91_86_CH_desc"] = "From 1977, consideration was given to a new paintwork, but it took a long time until the revision in 1986: The Olympic blue color applied in 1972 was no longer up-to-date. In the end, the decision was made in favor of the color gentian blue, which emphasizes the Bavarian home of the Glass Train. In 1995 the train was involved in an accident at Garmisch-Partenkirchen station and was then taken out of service and has since been parked in the Augsburg railway park in a non-operational state.\nLifted single-arm pantograph for operation in Switzerland.",
+
+        ["single_vehicle"] = "\n -- ! This rail car was a single piece. Please do not couple this vehicle but drive it as single car as in real world !",
+        ["towed_vehicle"] = "\n -- ! Just put this vehicle behind a loco for pulling, e.g. for drive along diesel tracks. Do not couple in a train !",
+	},
+	de = {
+		["mod_name"] = "DB BR ET 91 / BR 491",
+		["mod_desc"] = "Die Fahrzeuge der Baureihe ET 91 der Deutschen Reichsbahn (DRG) waren elektrische Triebwagen für Sonderverkehre. Die auch als Gläserner Zug bekannten Fahrzeuge waren rundum sowie an den Dachpartien verglast. Dadurch wurde den Fahrgästen eine besondere Rundumsicht geboten. Sie wurden nur zu Ausflugs- und Sonderfahrten eingesetzt, insbesondere in Süddeutschland und Österreich. Nach dem Krieg konnte noch ein Triebwagen von der Deutschen Bundesbahn übernommen werden. Ab 1968 wurde er als BR 491 001 geführt, nach einem Unfall im Jahre 1995 wurde er ausgemustert und ist heute im Museum erhalten.",
+
+        ["ET91_menu"] = "ET 91 / BR 491",
+        ["towed"] = " [geschleppt]",
+		["elT1998_name"] = "elT 1998 (1935)",
+		["elT1998_desc"] = "Gebaut wurden die Triebwagen 1935 in zwei Exemplaren als elT 1998 und 1999 von der Waggonfabrik Fuchs und AEG. Ab 1940 wurden die Baureihe als ET 91 geführt. ET 91 01 wurde während des Krieges in einem kleinen Lokschuppen eingemauert und blieb dadurch betriebsbereit erhalten.",
+        ["elT1999_name"] = "elT 1999 (1935)",
+		["elT1999_desc"] = "Gebaut wurden die Triebwagen 1935 in zwei Exemplaren als elT 1998 und 1999 von der Waggonfabrik Fuchs und AEG. Ab 1940 wurden die Baureihe als ET 91 geführt. ET 91 02 brannte bei einem Bombenangriff auf das Münchener Bahnbetriebswerk am 9. März 1943 vollständig aus und daraufhin am 9. Juli 1943 ausgemustert.",
+
+		["ET91_49_name"] = "ET 91 01 (1949)",
+		["ET91_49_desc"] = "Das Fahrzeug war fast unbeschädigt geblieben und kam nach dem Zweiten Weltkrieg zur Deutschen Bundesbahn. Es wurde aufgearbeitet und neu lackiert und wieder von München Hbf aus eingesetzt.",
+
+		["ET91_53_name"] = "ET 91 01 (1953)",
+		["ET91_53_desc"] = "Im Jahr 1953 wurde der ET 91 umgebaut, unter anderem wurde der Stromabnehmer und der elektrische Teil erneuert. So wurde auch ein drittes Spitzensignal angebaut sowie die Lackierung erneuert.",
+
+		["ET91_68_name"] = "491 001-4 (1968)",
+		["ET91_68_desc"] = "Mit Einführung der EDV-Nummern erhielt der ET 91 die Baureihenbezeichnung 491. Da nur ein einziger Triebzug existierte, gab es nur die Nummer 491 001-4.",
+
+        ["ET91_72_towed_name"] = "491 001-4 (1972)",
+		["ET91_72_name"] = "491 001-4 [DE] (1972)",
+		["ET91_72_desc"] = "Zur Olympiade in Deutschland erhielt der inzwischen unter der EDV 491 geführt ET 91 ein neues Farbkleid in olympiablau. Dies sollte den besonderen Status des Zuges unterstreichen. Außerdem wurde ein Stromabnehmer für den Betrieb in der Schweiz angebaut sowie neue Lampen.\nGehobener Scherenpanto für Betrieb in DE und AT.",
+        ["ET91_72_CH_name"] = "491 001-4 [CH] (1972)",
+		["ET91_72_CH_desc"] = "Zur Olympiade in Deutschland erhielt der inzwischen unter der EDV 491 geführt ET 91 ein neues Farbkleid in olympiablau. Dies sollte den besonderen Status des Zuges unterstreichen. Außerdem wurde ein Stromabnehmer für den Betrieb in der Schweiz angebaut sowie neue Lampen.\nGehobener Einholmpanto für Schweizer Betrieb.",
+
+		["ET91_86_towed_name"] = "491 001-4 (1986)",
+        ["ET91_86_name"] = "491 001-4 [DE] (1986)",
+		["ET91_86_desc"] = "Ab 1977 überlegte man über eine neue Lackierung, doch es dauerte lange bis zur Revision 1986: Die 1972 aufgebrachte Farbe Olympiablau war nicht mehr zeitgemäß. Man entschied sich schlußendlich für die Farbe Enzianblau, die die bayerische Heimat des Gläsernen Zuges betont. 1995 war der Zug im Bahnhof Garmisch-Partenkirchen in einen Unfall verwickelt und wurde daraufhin ausgemustert und ist seitdem nicht mehr betriebsfähig abgestellt im Bahnpark Augsburg.\nGehobener Scherenpanto für Betrieb in DE und AT.",
+        ["ET91_86_CH_name"] = "491 001-4 [CH] (1986)",
+		["ET91_86_CH_desc"] = "Ab 1977 überlegte man über eine neue Lackierung, doch es dauerte lange bis zur Revision 1986: Die 1972 aufgebrachte Farbe Olympiablau war nicht mehr zeitgemäß. Man entschied sich schlußendlich für die Farbe Enzianblau, die die bayerische Heimat des Gläsernen Zuges betont. 1995 war der Zug im Bahnhof Garmisch-Partenkirchen in einen Unfall verwickelt und wurde daraufhin ausgemustert und ist seitdem nicht mehr betriebsfähig abgestellt im Bahnpark Augsburg.\nGehobener Einholmpanto für Schweizer Betrieb.",
+
+        ["single_vehicle"] = "\n -- ! Der Triebwagen war ein Einzelstück. Das Fahrzeug bitte nicht kuppeln, sondern wie im echten Einsatz alleine fahren lassen !",
+        ["towed_vehicle"] = "\n -- ! Das Fahrzeug nur zum Schleppen hinter eine andere Lok hängen z.B. für Dieselstrecken, nicht in Züge einstellen !",
+	},
+}
+end
